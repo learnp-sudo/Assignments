1c7f706de8a40c4dbabb524775f8811a12ee94b2
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:27:26 +0530

    StringBuilder

---
+++ StringBuilderAppend.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.lang.StringBuilder;
+
+public class StringBuilderAppend {
+
+    public static void main(String[] args) {
+	StringBuilder s1=new StringBuilder("StringBuffer");
+            s1.append(" is a peer class of");
+        s1.append("String");
+        s1.append("that provides much of");
+        s1.append("the functionality of strings.");
+        System.out.println(s1);
+
+
+    }
+}
