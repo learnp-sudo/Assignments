e70952ba7ea32daadc28ecb50bb69d236e5da0f4
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:28:29 +0530

    StringBuilder

---
+++ StringBuilderInsert.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class StringBuilderInsert {
+
+    public static void main(String[] args) {
+	StringBuilder s1=new StringBuilder("It is used to at the specified index position.");
+    System.out.println("insert:"+s1.insert(14,"insert text "));
+    }
+}
