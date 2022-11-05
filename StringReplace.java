b56dbe7f5903c09004c614c02c92115788b93296
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:16:12 +0530

    StringBuffer

---
+++ StringReplace.java
@@ -0,0 +1,10 @@
+package com.company;
+import java.lang.StringBuffer;
+
+public class StringReplace {
+
+    public static void main(String[] args) {
+	StringBuffer s1=new StringBuffer("It is used to at the specified index position");
+    System.out.println("insert:"+s1.insert(14,"insert text "));
+    }
+}
