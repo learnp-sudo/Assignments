8385a16185022035984f582eec87470e42920d8c
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:14:51 +0530

    StringBuffer

---
+++ StringAppend.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.lang.StringBuffer;
+import java.lang.String;
+import java.util.Arrays;
+
+public class StringAppend {
+
+    public static void main(String[] args) {
+	StringBuffer s1=new StringBuffer("StringBuffer");
+    s1.append(" is a peer class of");
+    s1.append("String");
+    s1.append("that provides much of");
+    s1.append("the functionality of strings.");
+    System.out.println(s1);
+
+    }
+}
