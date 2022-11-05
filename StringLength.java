e673e51dc345d31f24ee570ff0cf608e12de5d61
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:08:45 +0530

    Strings

---
+++ StringLength.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class StringLength {
+
+    public static void main(String[] args) {
+        String s1="Hello World";
+        System.out.println(s1);
+        System.out.println("lenght of a String="+s1.length());
+    }
+}
