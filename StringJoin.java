7b4d0f4f8114e305996ae4ed8571d2f96b777c50
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:06:38 +0530

    String

---
+++ StringJoin.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class StringJoin {
+
+    public static void main(String[] args) {
+        String s1="Hello,";
+        String s2="How are you?";
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s1.concat(s2));
+    }
+}
