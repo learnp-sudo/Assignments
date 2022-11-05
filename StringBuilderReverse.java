f64b6d6cfa69eefac40edd4c1e4e3143bf19ff1d
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-05 13:29:44 +0530

    StringBuilder

---
+++ StringBuilderReverse.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class StringBuilderReverse {
+
+    public static void main(String[] args) {
+	StringBuilder s1=new StringBuilder("This method returns the reversed objecton which it was called");
+    System.out.println("reverse:"+s1.reverse());
+    }
+}
