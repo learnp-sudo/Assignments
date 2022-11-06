24bff1f1ae8d5fccb1d1ec371aebad683e9f91bb
learnp-sudo <115682000+learnp-sudo@users.noreply.github.com>
2022-11-06 18:17:26 +0530

    Java11Features up

--- VarDeclarations.java
+++ VarDeclarations.java
@@ -3,12 +3,12 @@ interface Demo
 {
     void demo(int a);
 }
-
+//var v=10; cannot be declared in global
 public class VarDeclarations {
-    //var p; var cannot be used
+    //var p; //var cannot be used in instance
     public static void main(String[] args)
     {
-    //var x; var cannot be used in instance
+    var x=23;
         Demo demo1=(var a)->{
             System.out.println(a);
         };
